6e841fdec621db4ae74d8376b9a81b827cffe5e7
Holman <holmanlopez20@gmail.com>
2025-10-28 10:35:30 -0500

    Se carga ultimas modificaciones

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -2,17 +2,27 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+// Angular Material modules
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.html',
   styleUrls: ['./contact.css'],
-  imports: [FormsModule, CommonModule],
   standalone: true,
+  imports: [
+    FormsModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   host: { 'ngSkipHydration': 'true' }
 })
 export class ContactComponent {
-  
+
   // Modelo para enlazar los campos del formulario
   contactData = {
     name: '',
